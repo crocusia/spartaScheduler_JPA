8da95fbe07ebfc87299d44f65b8df838a984af7b
crocusia <132359536+crocusia@users.noreply.github.com>
2025-04-03 15:50:06 +0900

    feat : 유저, 일정 Controller 추가

---
+++ src/main/java/com/example/schedulerjpa/controller/TaskController.java
@@ -0,0 +1,73 @@
+package com.example.schedulerjpa.controller;
+
+import com.example.schedulerjpa.dto.request.TaskCreateRequestDto;
+import com.example.schedulerjpa.dto.request.TaskUpdateRequestDto;
+import com.example.schedulerjpa.dto.response.TaskResponseDto;
+import com.example.schedulerjpa.dto.response.UserSessionDto;
+import com.example.schedulerjpa.service.TaskService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/tasks")
+@RequiredArgsConstructor //의존성 자동 주입
+public class TaskController {
+    private final TaskService taskService;
+    //일정 생성 - 유저는 본인 일정만 생성
+    @PostMapping
+    public ResponseEntity<TaskResponseDto> createTask(
+            @Valid @RequestBody TaskCreateRequestDto createDto,
+            @ModelAttribute("loginUser") UserSessionDto loginUser
+    ) {
+        //세션에 저장된 데이터를 사용하기 위해 매개변수로 넘김
+        TaskResponseDto taskResponseDto = taskService.saveTask(loginUser.getUserId(), createDto);
+        return ResponseEntity.ok(taskResponseDto);
+    }
+
+    //특정 조건을 만족하는 일정 전체 조회
+    @GetMapping
+    public ResponseEntity<List<TaskResponseDto>> findTasks(
+            @RequestParam(required = false) Optional<Long> userId,
+            @RequestParam(required = false, defaultValue = "") String updatedAt) {
+        Long userIdValue = userId.orElse(null);
+        List<TaskResponseDto> taskResponseDto = taskService.findTasks(userIdValue, updatedAt);
+        return ResponseEntity.ok(taskResponseDto);
+    }
+
+    //단일 일정 조회
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskResponseDto> findTaskById(@PathVariable Long id) {
+        TaskResponseDto taskResponseDto = taskService.findTaskById(id);
+        return ResponseEntity.ok(taskResponseDto);
+    }
+
+    //일정 수정 - 본인 일정만 가능
+    @PatchMapping("/{id}")
+    public ResponseEntity<TaskResponseDto> updateTask(
+            @PathVariable Long id,
+            @Valid @RequestBody TaskUpdateRequestDto updateDto,
+            @ModelAttribute("loginUser") UserSessionDto loginUser
+    ) {
+        TaskResponseDto taskResponseDto = taskService.updateTask(id, loginUser.getUserId(), updateDto);
+        return ResponseEntity.ok(taskResponseDto);
+    }
+
+    //일정 삭제 - 본일 일정만 가능
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteTask(
+            @PathVariable Long id,
+            @ModelAttribute("loginUser") UserSessionDto loginUser) {
+        //본인 일정 지우는데 편하게 지우게하자 (비밀번호 추가 검증은 유저 삭제에서만!)
+        //필터에서 이미 세션 검증한 상태
+        taskService.deleteTask(id, loginUser.getUserId());
+        return ResponseEntity.ok("일정 삭제 성공");
+    }
+
+}
---
+++ src/main/java/com/example/schedulerjpa/controller/UserController.java
@@ -0,0 +1,101 @@
+package com.example.schedulerjpa.controller;
+
+import com.example.schedulerjpa.dto.request.*;
+import com.example.schedulerjpa.dto.response.UserResponseDto;
+import com.example.schedulerjpa.dto.response.UserSessionDto;
+import com.example.schedulerjpa.dto.response.UserSignUpResponseDto;
+import com.example.schedulerjpa.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/signup") //회원가입
+    public ResponseEntity<UserSignUpResponseDto> signUp(@RequestBody UserSignUpRequestDto signUpDto){
+        UserSignUpResponseDto userSignUpResponseDto = userService.signUp(signUpDto);
+        return new ResponseEntity<>(userSignUpResponseDto, HttpStatus.CREATED);
+    }
+
+    //로그인
+    @PostMapping("/login")
+    public ResponseEntity<String> login(
+            @Valid @RequestBody LoginRequestDto loginRequest,
+            HttpServletRequest request
+    ){
+        HttpSession session = request.getSession();
+
+        if(session.getAttribute("loginUser") != null){
+            return ResponseEntity.badRequest().body("이미 로그인된 상태입니다.");
+        }
+
+        UserSessionDto loginUser = userService.login(loginRequest);
+
+        //세션에 사용자 정보 저장
+        session.setAttribute("loginUser", loginUser);
+        return ResponseEntity.ok("로그인 성공");
+    }
+
+    //로그아웃
+    @PostMapping("/logout")
+    public ResponseEntity<String> lohout( HttpServletRequest request ){
+        HttpSession session = request.getSession(false);
+        if(session != null){
+            session.invalidate();
+        }
+        return ResponseEntity.ok("로그아웃 성공");
+    }
+
+    //마이페이지
+    @PostMapping("/me")
+    public ResponseEntity<UserResponseDto> myPage(@ModelAttribute("loginUser") UserSessionDto loginUser){
+        UserResponseDto userResponseDto = userService.findUserById(loginUser.getUserId());
+        return ResponseEntity.ok((userResponseDto));
+    }
+
+    //유저 프로필 조회
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDto> findUserById(@PathVariable Long id) {
+        UserResponseDto responseDto = userService.findUserById(id);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    //유저 이름 변경
+    @PatchMapping("/me")
+    public ResponseEntity<UserResponseDto> updateUserName(
+            @Valid @RequestBody UserNameUpdateRequestDto updateDto,
+            @ModelAttribute("loginUser") UserSessionDto loginUser
+    ) {
+        UserResponseDto userResponseDto = userService.updateUserName(loginUser.getUserId(), updateDto);
+        return ResponseEntity.ok(userResponseDto);
+    }
+
+    //유저 비밀번호 변경
+    @PatchMapping("/me")
+    public ResponseEntity<String> updateUserPassword(
+            @Valid @RequestBody UserPasswordUpdateRequestDto updateDto,
+            @ModelAttribute("loginUser") UserSessionDto loginUser
+    ) {
+        userService.updateUserPassword(loginUser.getUserId(), updateDto);
+        return ResponseEntity.ok("비밀번호 변경 성공");
+    }
+
+    //유저 삭제
+    @PatchMapping("/me")
+    public ResponseEntity<String> deleteUser(
+            @Valid @RequestBody UserDeleteRequestDto deleteDto,
+            @ModelAttribute("loginUser") UserSessionDto loginUser
+    ) {
+        userService.deleteUser(loginUser.getUserId(), deleteDto);
+        return ResponseEntity.ok("유저 삭제 성공");
+    }
+
+}
