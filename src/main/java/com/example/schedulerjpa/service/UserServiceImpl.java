069e577ba85240696cd9b8d06e3c6902bc7b2b47
crocusia <132359536+crocusia@users.noreply.github.com>
2025-04-03 17:46:26 +0900

    feat : sql고유식별자에 AUTO_INCREMENT 추가

--- src/main/java/com/example/schedulerjpa/service/UserServiceImpl.java
+++ src/main/java/com/example/schedulerjpa/service/UserServiceImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 @Service
-@Transactional
+//@Transactional
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
